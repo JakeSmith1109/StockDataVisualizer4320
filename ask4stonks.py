192c0d313cf62f44f3568be60fb7e6a7530c07a0
Yuli Zhuo <yzhuo1122@icloud.com>
2023-10-22 16:01:02 -0500

    Added error handling for the prompts

--- ask4stonks.py
+++ ask4stonks.py
@@ -1,26 +1,68 @@
+ # import random things to make code work
+import re
+import datetime
+
 # Tell em
 print("Stonk Data Visualizer")
 print("----------------------")
 
 # Ask the user to enter the stock symbol for the company they want data for.
-stonkSymbol = input("Enter stonk symbol: ")
+stonkSymbol = input("\nEnter stonk symbol: ")
+while not re.match("^[A-Z0-9]+$", stonkSymbol):
+    print("Invalid input. Please enter a valid stock symbol.")
+    stonkSymbol = input("Enter stonk symbol: ")
 
 # Ask the user for the chart type they would like.
 print("Chart Types")
 print("------------")
 print("1. Bar\n2. Line")
-chartType = input("Enter the chart type you want (1, 2): ")
+chartType = input("\nEnter the chart type you want (1, 2): ")
+while chartType not in ['1', '2']:
+    print("Invalid input. Please enter either '1' for Bar or '2' for Line chart.")
+    chartType = input("Enter the chart type you want (1, 2): ")
 
 # Ask the user for the time series function they want the api to use.
 print("Select the Time Series of the chart you want to Generate")
 print("---------------------------------------------------------")
 print("1. Intraday\n2. Daily\n3. Weekly\n4. Monthly")
-timeSeries = input("Enter the time series option (1, 2, 3, 4): ")
+timeSeries = input("\nEnter the time series option (1, 2, 3, 4): ")
+while timeSeries not in ['1', '2', '3', '4']:
+    print("Invalid input. Please enter a valid time series option (1, 2, 3, 4).")
+    timeSeries = input("Enter the time series option (1, 2, 3, 4): ")
 
-# Ask the user for the beginning date in YYYY-MM-DD format.
-beginDate = input("Enter the start Date (YYYY-MM-DD): ")
+# get those damn dates
+while True:
+    beginDate = input("\nEnter the start Date (YYYY-MM-DD): ")
+    endDate = input("Enter the end Date (YYYY-MM-DD): ")
 
-# Ask the user for the end date in YYYY-MM-DD format. The end date should not be before the begin date.
-endDate = input("Enter the end Date (YYYY-MM-DD): ")
+    try:
+        # Ensure that the year has no more than 4 digits
+        if len(beginDate) != 10 or len(endDate) != 10:
+            print("Invalid date format. Please use YYYY-MM-DD format for dates.")
+            continue
+
+        begin_year = int(beginDate[:4])
+        end_year = int(endDate[:4])
+
+        # Ensure that the month has no more than 2 digits
+        begin_month = int(beginDate[5:7])
+        end_month = int(endDate[5:7])
+
+        # Ensure that the day has no more than 2 digits
+        begin_day = int(beginDate[8:10])
+        end_day = int(endDate[8:10])
+
+        if not (1000 <= begin_year <= 9999 and 1000 <= end_year <= 9999):
+            print("Year should have 4 digits.")
+        elif not (1 <= begin_month <= 12 and 1 <= end_month <= 12):
+            print("Month should have 2 digits or sbe in the range 1-12.")
+        elif not (1 <= begin_day <= 31 and 1 <= end_day <= 31):
+            print("Day should have 2 digits or be in the range 1-31.")
+        elif endDate < beginDate:
+            print("End date should not be before the begin date.")
+        else:
+            break
+    except ValueError:
+        print("Invalid date format. Please use YYYY-MM-DD format for dates.")
 
 # Generate a graph and open in the user’s default browser.
