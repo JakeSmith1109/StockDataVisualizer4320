6e565a9a9ce1b1cb587b6283e9da57df67f34a75
Clayton5456 <71726374+Clayton5456@users.noreply.github.com>
2023-10-23 22:53:38 -0500

    Added some explanation about my architecture

--- ask4stonks.py
+++ ask4stonks.py
@@ -81,7 +81,9 @@ elif timeSeries==2:
     data = r.json()
 
     print(data)
+    #Started architecture for filtering, the first line is to get to the actual data, which looks like a dictionary inside a dictionary 
     data=data["Time Series (Daily)"]
+    #this will go through each of the dictionaries for each time series (day,week,month), each of which uses the date as the key value
     for dat in data:
         if dat==beginDate:
             continue
